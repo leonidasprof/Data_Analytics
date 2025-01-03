c78c7cb6ce2076ad0ebd2663b79006424cca1ca6
Leônidas_Léo <prof.tenebroso@gmail.com>
2025-01-03 11:28:43 -0300

    adicionando a logo

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.image("logo.png", caption="Leônidas Business Intelligence")
+
 st.title('Números de corridas da UBER em Nova York 2024')
 
 DATE_COLUMN = 'date/time'
